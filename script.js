9406c5137f30d113dff7a2e471679125ac37148a
Robert Merrill <robgmerrill@gmail.com>
2020-04-05 10:29:18 -0700

    Project UI, generate random users with fetch async await, output users with forEach

---
+++ script.js
@@ -0,0 +1,65 @@
+const main = document.getElementById('main');
+const addUserBtn = document.getElementById('add-user');
+const doubleBtn = document.getElementById('double');
+const showMillionairs = document.getElementById('show-millionairs');
+const sortBtn = document.getElementById('sort');
+const calculateWealth = document.getElementById('calculae-wealth');
+
+let data = [];
+
+getRandomUser();
+getRandomUser();
+getRandomUser();
+
+// fetch random user and add money
+async function getRandomUser() {
+  const res = await fetch('https://randomuser.me/api');
+  const data = await res.json();
+  //   console.log(data);
+  const user = data.results[0];
+  const newUser = {
+    name: `${user.name.first} ${user.name.last}`,
+    money: Math.floor(Math.random() * 1000000),
+  };
+  console.log(newUser);
+  addData(newUser);
+}
+
+// Add new obj to data arr
+function addData(obj) {
+  data.push(obj);
+  updateDOM();
+  console.log('data', data);
+}
+
+// Update DOM
+function updateDOM(providedData = data) {
+  // Clear main div
+  main.innerHTML = '<h2><strong>Person</strong> Wealth</h2>';
+
+  // for(i = 0; i < providedData.length; i++) {
+  //     providedData[i]
+  // }
+
+  providedData.forEach((item) => {
+    // create element
+    const element = document.createElement('div');
+    // add person class
+    element.classList.add('person');
+    // innerHTL property for name and money
+    element.innerHTML = `<strong>${item.name}</strong> ${formatMoney(
+      item.money
+    )}`;
+    // append to main
+    main.appendChild(element);
+  });
+}
+
+// Format number as money - https://stackoverflow.com/questions/149055/how-to-format-numbers-as-currency-string
+
+function formatMoney(number) {
+  return `$` + number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+}
+
+// Event Listeners
+addUserBtn.addEventListener('click', getRandomUser);
